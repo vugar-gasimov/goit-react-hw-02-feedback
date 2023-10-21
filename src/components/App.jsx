49ff20cd9adcd996c724ff2379efffce5b498ed7
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-10-21 11:08:44 +0300

    FeedbackWidget begining

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { FeedbackWidget } from './Feedback-Widget/FeedbackWidget';
 export const App = () => {
   return (
     <div
@@ -7,10 +9,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <FeedbackWidget />
     </div>
   );
 };
---
+++ src/components/Feedback-Widget/FeedbackWidget.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+}
