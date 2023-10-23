a5f945e98d9b4cdc3c0eecde1f63ededb6124b19
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-10-24 00:52:04 +0300

    +++

--- src/components/Feedback-Widget/FeedbackOptions.jsx
+++ src/components/Feedback-Widget/FeedbackOptions.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FeedbackContainer, FeedbackButton } from '../AppStyled.Styled';
-import { nanoid } from 'nanoid';
+
 import PropTypes from 'prop-types';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
